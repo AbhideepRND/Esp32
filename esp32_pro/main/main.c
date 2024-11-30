d412f3a2791d90f8fa065844c6d79ac2cddaead5
Abhidep Bakshi <abhidep@sentientgeeks.com>
2024-11-30 18:34:55 +0530

    Set the workspace in VSCode

--- esp32_pro/main/main.c
+++ esp32_pro/main/main.c
@@ -1,25 +1,22 @@
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "nvs_flash.h"
 
 #include "reg_neo_pix.h"
 #include <esp_netif.h>
 #include <esp_now.h>
 
-void app_main(void)
-{
+void app_main(void) {
 
-    while (true) {
-    	rgb_led_wifi_app_started();
+	while (true) {
+		rgb_led_wifi_app_started();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 		rgb_led_http_server_started();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 		rgb_led_wifi_connect();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+	}
 }
-
